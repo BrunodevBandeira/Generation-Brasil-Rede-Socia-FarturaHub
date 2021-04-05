ec985600bf63e0bd59823a5c59f585fc97637281
Thales Santana <generationt2h@gmail.com>
2021-04-05 19:32:46 -0300

    Correção do código responsavel pela Data e Hora

--- farturahub/farturahub/src/main/java/com/hub/farturahub/model/Postagem.java
+++ farturahub/farturahub/src/main/java/com/hub/farturahub/model/Postagem.java
@@ -1,15 +1,12 @@
 package com.hub.farturahub.model;
 
-import java.util.Date;
-
+import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -46,8 +43,7 @@ public class Postagem {
 	@Size(min = 10, max = 100)
 	private String contato;
 	
-	@Temporal(TemporalType.TIMESTAMP)
-    private Date data = new java.sql.Date(System.currentTimeMillis());
+	private LocalDateTime data = LocalDateTime.now();
 	
 	//relacionamento
 	@ManyToOne
@@ -113,11 +109,11 @@ public class Postagem {
 		this.contato = contato;
 	}
 
-	public Date getData() {
+	public LocalDateTime getData() {
 		return data;
 	}
 
-	public void setData(Date data) {
+	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
 	
