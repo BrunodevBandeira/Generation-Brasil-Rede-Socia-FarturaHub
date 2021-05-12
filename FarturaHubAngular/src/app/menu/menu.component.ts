6444eb83c3b868ffd957d995280b4d0bdf4a7a3c
Thales Santana <generationt2h@gmail.com>
2021-05-12 09:31:28 -0300

    Atualização das paginas de entrada e cadastro

--- FarturaHubAngular/src/app/menu/menu.component.ts
+++ FarturaHubAngular/src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  /*public deslogar(){ 
+    this.router.navigate(["/"]);
+  }*/
 }
